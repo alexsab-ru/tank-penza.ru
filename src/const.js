2342e639177d60d74f4cf47516d6cba9e8c9aa6f
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-06-19 09:52:47 +0500

    Update const.js

--- src/const.js
+++ src/const.js
@@ -1,14 +1,14 @@
 import { phoneFormat } from '@/js/utils/numbers.format';
 // Название сайта по умолчанию
-export const SITE_NAME = 'Название сайта';
+export const SITE_NAME = 'TANK SAMARA';
 // Юр лицо
 export const LEGAL_ENTITY = 'ООО «Юридическое название»';
 // ИНН
 export const LEGAL_INN = '1234567890';
 // Город
-export const LEGAL_CITY = 'г. Город';
+export const LEGAL_CITY = 'г. Самара';
 // где? в Городе
-export const LEGAL_CITY_WHERE = 'Городе';
+export const LEGAL_CITY_WHERE = 'Самаре';
 // Описание сайта по умолчанию
 export const SITE_DESCR = 'Официальный дилерский центр';
 // Имя пользователя в Телегам
@@ -16,7 +16,7 @@ export const TELEGRAM = '';
 // Телефон по умолчанию
 export const PHONE = '+7 (999) 000-00-00';
 // Бренд
-export const BRAND = 'Baic';
+export const BRAND = 'TANK';
 // Конечное время для таймера
 //string 2024-04-26 or 2024-04-26 23:59:59 or December 31 2015 or December 31 2015 23:59:59 GMT+02:00
 export const TIMER_ENDTIME = '';
@@ -26,8 +26,8 @@ export const LINK_WIDGET = 'https://yandex.ru/map-widget/v1/-/';
 export const LINK_WIDGET_ORGNIZATION = '';
 // Ссылки под хедером
 export const LINKS_MENU = [
-	{url: 'cars/', name: 'Авто в наличии'},
-	{url: 'special-offers/', name: 'Спецпредложения'},
+	// {url: 'cars/', name: 'Авто в наличии'},
+	// {url: 'special-offers/', name: 'Спецпредложения'},
 	{url: 'news/', name: 'Новости'},
 	{url: 'test-drive/', name: 'Запись на тест-драйв'},
 	{url: 'service-request/', name: 'Запись на сервис'},
